bb0aae750b69e35c392333c31859d262cc609769
Igor Santos <igor.sagitarius@gmail.com>
2020-10-14 19:35:44 -0300

    Criação do banco de dados,das rotas e abstração do codigo

---
+++ backend/src/routes.ts
@@ -0,0 +1,9 @@
+import {Router} from 'express';
+import OrphanagesController from './controllers/orphanagesController'
+
+const routes = Router();
+
+routes.post('/orphanages', OrphanagesController.create)
+
+
+export default routes;
