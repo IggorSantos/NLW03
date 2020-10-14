212903c85771892278b5c0a0bbc7337c05debf8c
Igor Santos <igor.sagitarius@gmail.com>
2020-10-14 20:41:19 -0300

    Criação das rotas para listagem de orfanatos e busca de orfanato

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,8 @@ import OrphanagesController from './controllers/orphanagesController'
 
 const routes = Router();
 
+routes.get('/orphanages', OrphanagesController.index)
+routes.get('/orphanages/:id', OrphanagesController.show)
 routes.post('/orphanages', OrphanagesController.create)
 
 
